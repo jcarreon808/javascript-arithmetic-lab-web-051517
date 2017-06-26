a9fa122e1332f0a4a6e1993a7a353afeb2efdceb
Jan Phillip Carreon <janphillipcarreon@yahoo.com>
2017-06-26 10:32:44 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b){
+  var sum = a + b;
+  return sum;
+}
+
+function subtract(a,b){
+  var difference = a - b;
+  return difference;
+}
+
+function multiply(a,b){
+  var product = a * b;
+  return product;
+}
+
+function divide(a,b){
+  var quotient = a / b;
+  return quotient;
+}
+
+function inc(n){
+  var inc = n + 1;
+  return inc;
+}
+
+function dec(n){
+  var dec = n -1;
+  return dec;
+}
+
+function makeInt(n){
+  var num = parseInt(n,10);
+  return num;
+}
+
+function preserveDecimal(n){
+  var decimalNum = parseFloat(n);
+  return decimalNum;
+}
